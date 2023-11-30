e5259b9ee0564b9533fed052e6ec363115e6178e
Sibusiso Khoza <sbusiso.khoza5@gmail.com>
2023-12-01 01:07:14 +0200

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -2,8 +2,7 @@
 
 /**
  * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node of the tree to measure the height
- *
+ * @tree: Pointer to the root node of the tree to measure the height.
  * Return: Height of the tree, 0 if @tree is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
@@ -16,13 +15,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-	return (1 + (left_height > right_height ? left_height : right_height));
+	return ((left_height > right_height ? left_height : right_height) + 1);
 }
 
 /**
  * binary_tree_size - Measures the size of a binary tree
- * @tree: Pointer to the root node of the tree to measure the size
- *
+ * @tree: Pointer to the root node of the tree to measure the size.
  * Return: Size of the tree, 0 if @tree is NULL
  */
 size_t binary_tree_size(const binary_tree_t *tree)
@@ -30,14 +28,13 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
 
 /**
  * binary_tree_is_perfect - Checks if a binary tree is perfect
  * @tree: Pointer to the root node of the tree to check
- *
- * Return: 1 if perfect, 0 otherwise
+ * Return: 1 if the tree is perfect, 0 otherwise
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
@@ -47,7 +44,5 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if ((1 << height) - 1 == size)
-		return (1);
-	return (0);
+	return ((1 << height) - 1 == size);
 }
