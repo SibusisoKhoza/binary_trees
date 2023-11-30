c84b4dc1506a79e03d93791b6f4c66994c465d30
Sibusiso Khoza <sbusiso.khoza5@gmail.com>
2023-12-01 00:28:06 +0200

    Test

--- binary_trees.h
+++ binary_trees.h
@@ -29,11 +29,11 @@ typedef struct binary_tree_s heap_t;
 void binary_tree_print(const binary_tree_t *);
 
 /* Function prototypes */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value); /* binary_tree_node */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value); /* binary_tree_insert_left */
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value); /* binary_tree_insert_right */
-void binary_tree_delete(binary_tree_t *tree); /* binary_tree_delete */
-int binary_tree_is_leaf(const binary_tree_t *node); /* binary_tree_is_leaf */
-int binary_tree_is_root(const binary_tree_t *node); /* binary_tree_is_root */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
 
 #endif /* BINARY_TREES_H */
