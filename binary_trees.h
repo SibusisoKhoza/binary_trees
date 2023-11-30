a718b5d9fff3bac6d851b342d027d93a44ad975d
Sibusiso Khoza <sbusiso.khoza5@gmail.com>
2023-12-01 00:20:55 +0200

    Leaf

--- binary_trees.h
+++ binary_trees.h
@@ -33,5 +33,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value); /* binary_tre
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value); /* binary_tree_insert_left */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value); /* binary_tree_insert_right */
 void binary_tree_delete(binary_tree_t *tree); /* binary_tree_delete */
+int binary_tree_is_leaf(const binary_tree_t *node); /* binary_tree_is_leaf */
 
 #endif /* BINARY_TREES_H */
