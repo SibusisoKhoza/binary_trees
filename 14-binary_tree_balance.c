343b281959e6b551a3bb9621b0ac1c25078ebdea
Sibusiso Khoza <sbusiso.khoza5@gmail.com>
2023-12-01 03:48:51 +0200

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -4,32 +4,46 @@
  * binary_tree_height - Measures the height of a binary tree
  * @tree: Pointer to the root node of the tree to measure the height
  *
- * Return: Height of the tree, 0 if @tree is NULL
+ * Return: Height of the tree
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+    size_t left_height, right_height;
 
-	if (tree == NULL)
-		return (0);
+    if (tree == NULL)
+        return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
 
-	return ((left_height > right_height ? left_height : right_height) + 1);
+    return (1 + (left_height > right_height ? left_height : right_height));
 }
 
 /**
- * binary_tree_balance - Measures the balance factor of a binary tree
- * @tree: Pointer to the root node of the tree to measure the balance factor
+ * binary_tree_is_perfect - Checks if a binary tree is perfect
+ * @tree: Pointer to the root node of the tree to check
  *
- * Return: Balance factor, 0 if @tree is NULL
+ * Return: 1 if perfect, 0 otherwise
  */
-int binary_tree_balance(const binary_tree_t *tree)
+int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+    size_t left_height, right_height;
 
-	return ((int)binary_tree_height(tree->left) -
-			(int)binary_tree_height(tree->right));
+    if (tree == NULL)
+        return (0);
+
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
+
+    if (left_height == right_height)
+    {
+        if (tree->left == NULL && tree->right == NULL)
+            return (1);
+
+        if (binary_tree_is_perfect(tree->left) &&
+            binary_tree_is_perfect(tree->right))
+            return (1);
+    }
+
+    return (0);
 }
