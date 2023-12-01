165c9118e7e6dcfd3b32cfa56c6c5103aa882179
Sibusiso Khoza <sbusiso.khoza5@gmail.com>
2023-12-01 03:57:18 +0200

    Update 100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,47 +1,31 @@
 #include "binary_trees.h"
 
 /**
- * binary_trees_ancestor - Find lowest common ancestor of two nodes
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes
  * @first: Pointer to the first node
  * @second: Pointer to the second node
- * Return: Pointer to the lowest common ancestor, or NULL if not found
+ *
+ * Return: Pointer to the lowest common ancestor node, or NULL if not found
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
-	const binary_tree_t *ancestor;
+	const binary_tree_t *tmp1, *tmp2;
 
-	if (!first || !second)
+	if (first == NULL || second == NULL)
 		return (NULL);
 
-	ancestor = first;
-	while (ancestor)
+	tmp1 = first;
+	while (tmp1)
 	{
-		if (binary_tree_is_ancestor(ancestor, second))
-			return ((binary_tree_t *)ancestor);
-		ancestor = ancestor->parent;
+		tmp2 = second;
+		while (tmp2)
+		{
+			if (tmp1 == tmp2)
+				return ((binary_tree_t *)tmp1);
+			tmp2 = tmp2->parent;
+		}
+		tmp1 = tmp1->parent;
 	}
-
 	return (NULL);
-}
-
-/**
- * binary_tree_is_ancestor - Check if a node is an ancestor of another
- * @ancestor: The potential ancestor node
- * @node: The potential descendant node
- * Return: 1 if ancestor is a true ancestor of node, 0 otherwise
- */
-int binary_tree_is_ancestor(const binary_tree_t *ancestor, const binary_tree_t *node)
-{
-	if (!node)
-		return (0);
-
-	while (node)
-	{
-		if (node == ancestor)
-			return (1);
-		node = node->parent;
-	}
-
-	return (0);
 }
 
