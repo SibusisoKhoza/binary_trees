38c96e6535bd6ba158aa491f754cba3883f82674
Sibusiso Khoza <sbusiso.khoza5@gmail.com>
2023-12-01 02:58:30 +0200

    Update 101-binary_tree_levelorder.c

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -8,32 +8,32 @@
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
-        return;
+	if (tree == NULL || func == NULL)
+		return;
 
-    /* Create a queue to store nodes */
-    const binary_tree_t **queue = malloc(sizeof(binary_tree_t *) * binary_tree_size(tree));
-    size_t front = 0, rear = 0;
-    const binary_tree_t *current;
+	/* Create a queue to store nodes */
+	const binary_tree_t **queue = malloc(sizeof(binary_tree_t *) * binary_tree_size(tree));
+	size_t front = 0, rear = 0;
+	const binary_tree_t *current;
 
-    if (queue == NULL)
-        return;
+	if (queue == NULL)
+		return;
 
-    queue[rear++] = tree;
+	queue[rear++] = tree;
 
-    while (front < rear)
-    {
-        current = queue[front++];
-        func(current->n);
+	while (front < rear)
+	{
+		current = queue[front++];
+		func(current->n);
 
-        /* Enqueue left child */
-        if (current->left != NULL)
-            queue[rear++] = current->left;
+		/* Enqueue left child */
+		if (current->left != NULL)
+			queue[rear++] = current->left;
 
-        /* Enqueue right child */
-        if (current->right != NULL)
-            queue[rear++] = current->right;
-    }
+		/* Enqueue right child */
+		if (current->right != NULL)
+			queue[rear++] = current->right;
+	}
 
-    free(queue);
+	free(queue);
 }
